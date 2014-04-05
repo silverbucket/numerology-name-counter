55efa25bca8ac61dd5924d5de646081a4b4925cc
Nick Jennings <nick@silverbucket.net>
2014-04-05 03:20:24 +0200

    build update

--- www/res/js/app.js
+++ www/res/js/app.js
@@ -1,5 +1,50 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-(function (__dirname){
-var nnc = require(__dirname + '/nnc');
-}).call(this,"/")
+var nnc = require('./nnc');
+},{"./nnc":2}],2:[function(require,module,exports){
+var CHAR_MAP = {
+  'a' : 1,
+  'b' : 2,
+  'c' : 3,
+  'd' : 4,
+  'e' : 5,
+  'f' : 6,
+  'g' : 7,
+  'h' : 8,
+  'i' : 9,
+  'j' : 10,
+  'k' : 11,
+  'l' : 12,
+  'm' : 13,
+  'n' : 14,
+  'o' : 15,
+  'p' : 16,
+  'q' : 17,
+  'r' : 18,
+  's' : 19,
+  't' : 20,
+  'u' : 21,
+  'v' : 22,
+  'w' : 23,
+  'x' : 24,
+  'y' : 25,
+  'z' : 26
+};
+
+var hebrew = function (word) {
+  var total = 0;
+  for (var i = 0, len = word.length; i < len; i = i + 1) {
+    total = total + CHAR_MAP[word.charAt(i).toLowerCase()];
+  }
+  return total;
+};
+
+var english = function () {};
+
+var simple = function () {};
+
+module.exports = {
+  hebrew: hebrew,
+  english: english,
+  simple: simple
+};
 },{}]},{},[1])
